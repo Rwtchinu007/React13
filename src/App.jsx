c1561c725b9923752a972ac6637bd34257088b8f
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-03-06 19:45:25 +0530

    createBrowserRouter and RouterProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,66 @@
-import React, { Children } from 'react'
-import Navbar from './components/Navbar'
-import { createBrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Courses from './pages/Courses'
-import Kodex from './pages/Kodex'
-import Cohort from './pages/Cohort'
-import Footer from './components/Footer'
-import Layout from './components/Layout'
+import React, { Children } from "react";
+import Navbar from "./components/Navbar";
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Courses from "./pages/Courses";
+import Kodex from "./pages/Kodex";
+import Cohort from "./pages/Cohort";
+import Footer from "./components/Footer";
+import Layout from "./components/Layout";
 
 const App = () => {
-  // const allRoutes = createBrowserRouter([{
-  //   path:'/',
-  //   element:<Layout/>,
-  //   children:[{
-  //     index:true,
-  //     element:<Home/>
-  //   },{
-  //     index:true,
-  //     element:<About/>
-  //   },{}]
-  // }])
+  const allRoutes = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "about",
+          element: <About />,
+        },
+        {
+          path: "courses",
+          element: <Courses />,
+          children: [
+            {
+              path: "kodex",
+              element: <Kodex />,
+            },
+            {
+              path: "cohort",
+              element: <Cohort />,
+            },
+          ],
+        },
+      ],
+    },
+  ]);
   return (
-    <div className=''>
-      <Navbar />
-      <Routes>
+    <div className="">
+      {/* <Navbar /> */}
+      {/* <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/about' element={<About />}/>
         <Route path='/courses' element={<Courses />}>
         <Route path='/courses/kodex' element={<Kodex/>}/>
         <Route path='/courses/cohort' element={<Cohort/>}/>
         </Route>
-      </Routes>
+      </Routes> */}
+      <RouterProvider router={allRoutes} />
 
-      <Footer/>
-      </div>
-  )
-}
+      {/* <Footer/> */}
+    </div>
+  );
+};
 
-export default App
+export default App;
