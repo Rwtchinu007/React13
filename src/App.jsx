eefd20eb276a6739e3fee84019b8ed0bb6592eef
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-03-06 16:56:20 +0530

    React-router-dom- detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Children } from 'react'
 import Navbar from './components/Navbar'
 import { createBrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom'
 import Home from './pages/Home'
@@ -7,37 +7,31 @@ import Courses from './pages/Courses'
 import Kodex from './pages/Kodex'
 import Cohort from './pages/Cohort'
 import Footer from './components/Footer'
+import Layout from './components/Layout'
 
 const App = () => {
-  const allRoutes = createBrowserRouter([{
-    path:'/',
-    element:<Home/>
-  },{
-    path:'/about',
-    element:<About/>
-  },{
-    path:'/courses', 
-    element:<Courses/>,
-    children=[{
-      path:'kodex',
-      element:<Kodex/>
-    },{
-      path:'cohort',
-      element:<Cohort/>
-    }]
-  }]) 
+  // const allRoutes = createBrowserRouter([{
+  //   path:'/',
+  //   element:<Layout/>,
+  //   children:[{
+  //     index:true,
+  //     element:<Home/>
+  //   },{
+  //     index:true,
+  //     element:<About/>
+  //   },{}]
+  // }])
   return (
     <div className=''>
       <Navbar />
-      {/* <Routes>
+      <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/about' element={<About />}/>
         <Route path='/courses' element={<Courses />}>
         <Route path='/courses/kodex' element={<Kodex/>}/>
         <Route path='/courses/cohort' element={<Cohort/>}/>
         </Route>
-      </Routes> */}
-      <RouterProvider 
+      </Routes>
 
       <Footer/>
       </div>
